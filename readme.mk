15619861eb4266acfd2d637d0384807355133c4a
qcretenet <qcretenet@student.ethz.ch>
2022-11-24 11:21:00 +0100

    Readme started

---
+++ readme.mk
@@ -0,0 +1,13 @@
+# Machine Learning Based Elo Estimator : Project Overview
+* Create a tool aloowing chess players to estimate the elo of an unranked player based on their performance during a tournament. Can also be used if the player is suspected to be underrated.
+* Download the data made available by lichess.com (can be found on kaggle : https://www.kaggle.com/datasets/datasnaek/chess?resource=download)
+* Engineered features from the information available in the database
+* Optimized Linear, Lasso, Ridge, Random Forest, and SVM Regressors making use of GridSearchCV to reach the best model.
+
+<hr/>
+
+## Ressources
+* **Python Version :** 3.8.10
+* **Packages :** Numpy / Matplotlib / Pandas / Scikit-Learn
+
+<hr/>
